6fa42bcb1d25e487f591e79ad72837a7dd5decc0
dev47929 <dev47929@gmail.com>
2026-06-14 08:23:20 +0530

    Alot of changes

--- demo/src/main/java/com/dev/featureflag/entity/User.java
+++ demo/src/main/java/com/dev/featureflag/entity/User.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Entity
 @Table(name = "user_details")
 @NoArgsConstructor
-public class UserDetails implements UserDetails {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
